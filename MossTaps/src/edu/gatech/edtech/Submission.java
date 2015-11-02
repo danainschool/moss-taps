e0848eacd6906653973af0d1a9fb30a00cf39425
Dana Sheahen <danasheahen@gatech.edu>
2015-11-01 22:35:13 -0600

    up

--- MossTaps/src/edu/gatech/edtech/Submission.java
+++ MossTaps/src/edu/gatech/edtech/Submission.java
@@ -1,14 +1,28 @@
 package edu.gatech.edtech;
 
+import java.io.File;
+import java.util.Collection;
+
+import org.apache.commons.io.FileUtils;
+
 public class Submission {
 	String language;
 	String server = "moss.stanford.edu";
 	String port = "7690";
 	String userID;
+	String parentFolder;
 	
 
-	public Submission() {
-		// TODO Auto-generated constructor stub
+	public Submission(String folder, String language, String userID) {
+		this.language = language;
+		this.userID = userID;
+		this.parentFolder = folder;				
 	}
-
+	
+	public String submit(){
+		Collection<File> files = FileUtils.listFiles(new File(parentFolder), new String[] {language}, true);
+		
+		return null;
+		
+	}
 }
